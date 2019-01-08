4c6d627dce8e4eb041323b0cc7106eaf9e447c19
aojinadu <aojinadu@DEVOPS-22.unionbank-ng.unionbankgroup.local>
2019-01-08 14:15:03 +0100

    Added Logger Class

--- BVNValidationService2/src/main/java/com/ubn/bvnv2/BVNValidation.java
+++ BVNValidationService2/src/main/java/com/ubn/bvnv2/BVNValidation.java
@@ -1,5 +1,7 @@
 package com.ubn.bvnv2;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +14,7 @@ import com.ubn.bvnv2.service.BVNService;
 
 @RestController
 public class BVNValidation {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	BVNService serv;
@@ -20,13 +23,14 @@ public class BVNValidation {
 	
 	@PostMapping(value="/verifyBVN")
 	public @ResponseBody CustomerDetails Verify(@RequestBody BVNNumber BvnNumber) {
-		System.out.println(BvnNumber);
 		customer = new CustomerDetails();
 		try {
+			logger.info("BVN Number : " + BvnNumber);
 			customer = serv.getBVNdetails(BvnNumber);
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
+			logger.error("Error encountered", e);
 		}
 		return customer;
 	}
--- BVNValidationService2/src/main/java/com/ubn/bvnv2/BvnValidationService2Application.java
+++ BVNValidationService2/src/main/java/com/ubn/bvnv2/BvnValidationService2Application.java
@@ -1,10 +1,11 @@
 package com.ubn.bvnv2;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BvnValidationService2Application {
+public class BvnValidationService2Application{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BvnValidationService2Application.class, args);
