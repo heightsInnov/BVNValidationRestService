633a974922f3a58cded2efc48e35926a371025ee
aojinadu <aojinadu@DEVOPS-22.unionbank-ng.unionbankgroup.local>
2019-01-28 13:25:18 +0100

    method overload

--- BVNValidationService2/src/main/java/com/ubn/bvnv2/BVNValidation.java
+++ BVNValidationService2/src/main/java/com/ubn/bvnv2/BVNValidation.java
@@ -3,6 +3,7 @@ package com.ubn.bvnv2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,9 +23,24 @@ public class BVNValidation {
 	CustomerDetails customer;
 	
 	
-	@PostMapping(value="/verifyBVN", produces= {"application/json", "application/xml"})
+	@PostMapping(value="/verifyBVN")
 	public @ResponseBody CustomerDetails Verify(@RequestBody BVNNumber BvnNumber) {
 		logger.info("BVN Service Starts Now***********");
+		customer = new CustomerDetails();
+		try {
+			customer = serv.getBVNdetails(BvnNumber);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			logger.error("Error encountrered in "+ e.getStackTrace().getClass().getEnclosingMethod().getName(), e);
+		}
+		return customer;
+	}
+	
+	@PostMapping(value="/verifyBVN",consumes= {"application/xml"}, produces= {"application/xml"})
+	public @ResponseBody CustomerDetails VerifyOF(@RequestBody BVNNumber BvnNumber) {
+		logger.info("BVN Service Starts Now***********");
+		System.out.println("BVN Number is "+BvnNumber.getBvn());
 		customer = new CustomerDetails();
 		try {
 			customer = serv.getBVNdetails(BvnNumber);
